3d4cecb5cd6d02a3688945e0278c0b2420dc6756
abe157 <abenezertgs96@gmail.com>
2017-11-24 12:25:38 +0000

    Test2 now has the weather with tokens

--- test2.py
+++ test2.py
@@ -24,8 +24,8 @@ weather_lang = 'en'
 weather_unit = 'us'
 # Set this if IP location lookup does not work for you (must be a string)
 # Set this if IP location lookup does not work for you (must be a string)
-latitude = None 
-longitude = None 
+latitude = '37.8267'
+longitude = '-122.4233'
 xlarge_text_size = 40#94
 large_text_size = 30#48
 medium_text_size = 20#28
@@ -99,6 +99,117 @@ class Clock(Frame):
             self.timeLbl.after(200, self.tick)
 
 
+class Weather(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, bg='black')
+        self.temperature = ''
+        self.forecast = ''
+        self.location = ''
+        self.currently = ''
+        self.icon = ''
+        self.degreeFrm = Frame(self, bg="black")
+        self.degreeFrm.pack(side=TOP, anchor=W)
+        self.temperatureLbl = Label(self.degreeFrm, font=('Helvetica', xlarge_text_size), fg="white", bg="black")
+        self.temperatureLbl.pack(side=LEFT, anchor=N)
+        self.iconLbl = Label(self.degreeFrm, bg="black")
+        self.iconLbl.pack(side=LEFT, anchor=N, padx=20)
+        self.currentlyLbl = Label(self, font=('Helvetica', medium_text_size), fg="white", bg="black")
+        self.currentlyLbl.pack(side=TOP, anchor=W)
+        self.forecastLbl = Label(self, font=('Helvetica', small_text_size), fg="white", bg="black")
+        self.forecastLbl.pack(side=TOP, anchor=W)
+        self.locationLbl = Label(self, font=('Helvetica', small_text_size), fg="white", bg="black")
+        self.locationLbl.pack(side=TOP, anchor=W)
+        self.get_weather()
+
+    def get_ip(self):
+        try:
+            ip_url = "http://jsonip.com/"
+            req = requests.get(ip_url)
+            ip_json = json.loads(req.text)
+            return ip_json['ip']
+        except Exception as e:
+            traceback.print_exc()
+            return "Error: %s. Cannot get ip." % e
+
+    def get_weather(self):
+        try:
+
+            if latitude is None and longitude is None:
+                # get location
+                location_req_url = "http://freegeoip.net/json/%s" % self.get_ip()
+                r = requests.get(location_req_url)
+                location_obj = json.loads(r.text)
+
+                lat = location_obj['latitude']
+                lon = location_obj['longitude']
+
+                location2 = "%s, %s" % (location_obj['city'], location_obj['region_code'])
+
+                # get weather
+                weather_req_url = "https://api.darksky.net/forecast/%s/%s,%s?lang=%s&units=%s" % (weather_api_token, lat,lon,weather_lang,weather_unit)
+            else:
+                location2 = ""
+                # get weather
+                weather_req_url = "https://api.darksky.net/forecast/%s/%s,%s?lang=%s&units=%s" % (weather_api_token, latitude, longitude, weather_lang, weather_unit)
+
+            r = requests.get(weather_req_url)
+            weather_obj = json.loads(r.text)
+
+            degree_sign= u'\N{DEGREE SIGN}'
+            temperature2 = "%s%s" % (str(int(weather_obj['currently']['temperature'])), degree_sign)
+            currently2 = weather_obj['currently']['summary']
+            forecast2 = weather_obj["hourly"]["summary"]
+
+            icon_id = weather_obj['currently']['icon']
+            icon2 = None
+
+            if icon_id in icon_lookup:
+                icon2 = icon_lookup[icon_id]
+
+            if icon2 is not None:
+                if self.icon != icon2:
+                    self.icon = icon2
+                    image = Image.open(icon2)
+                    image = image.resize((40, 40), Image.ANTIALIAS)
+                    image = image.convert('RGB')
+                    photo = ImageTk.PhotoImage(image)
+
+                    self.iconLbl.config(image=photo)
+                    self.iconLbl.image = photo
+            else:
+                # remove image
+                self.iconLbl.config(image='')
+
+            if self.currently != currently2:
+                self.currently = currently2
+                self.currentlyLbl.config(text=currently2)
+            if self.forecast != forecast2:
+                self.forecast = forecast2
+                self.forecastLbl.config(text=forecast2)
+            if self.temperature != temperature2:
+                self.temperature = temperature2
+                self.temperatureLbl.config(text=temperature2)
+            if self.location != location2:
+                if location2 == ", ":
+                    self.location = "Cannot Pinpoint Location"
+                    self.locationLbl.config(text="Cannot Pinpoint Location")
+                else:
+                    self.location = location2
+                    self.locationLbl.config(text=location2)
+        except Exception as e:
+            traceback.print_exc()
+            print "Error: %s. Cannot get weather." % e
+
+        self.after(600000, self.get_weather)
+
+    @staticmethod
+    def convert_kelvin_to_fahrenheit(kelvin_temp):
+        return 1.8 * (kelvin_temp - 273) + 32
+
+
+
+            
+
 #create the window
 class FullWindow:
     def __init__(self):
@@ -115,8 +226,10 @@ class FullWindow:
         #self.tk.bind("<Escape>", self.end_fullscreen)
         # clock
         self.clock = Clock(self.topFrame)
-        self.clock.pack(side=RIGHT, anchor=N, padx=100, pady=60)
-
+        self.clock.pack(side=RIGHT, anchor=N, padx=10, pady=10)
+        # weather
+        self.weather = Weather(self.topFrame)
+        self.weather.pack(side=LEFT, anchor=N, padx=10, pady=10)
 
 
 
