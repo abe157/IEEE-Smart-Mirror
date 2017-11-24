1830cbff3940892260da06341aa51c6d1f87553b
abe157 <abenezertgs96@gmail.com>
2017-11-24 12:04:04 +0000

    test2 time now included

--- test2.py
+++ test2.py
@@ -1,13 +1,126 @@
 #simple GUI
 
 from Tkinter import *
+import locale
+import threading
+import time
+import requests
+import json
+import traceback
+import feedparser
+
+from PIL import Image, ImageTk
+from contextlib import contextmanager
+
+LOCALE_LOCK = threading.Lock()
+
+ui_locale = '' # e.g. 'fr_FR' fro French, '' as default
+time_format = 12 # 12 or 24
+date_format = "%b %d, %Y" # check python doc for strftime() for options
+news_country_code = 'us'
+# WEATHER: account from https://darksky.net/ 
+weather_api_token = '7d1611b46723c93de239aab1676c7129' 
+weather_lang = 'en' 
+weather_unit = 'us'
+# Set this if IP location lookup does not work for you (must be a string)
+# Set this if IP location lookup does not work for you (must be a string)
+latitude = None 
+longitude = None 
+xlarge_text_size = 20#94
+large_text_size = 15#48
+medium_text_size = 10#28
+small_text_size = 5#18
+
+@contextmanager
+def setlocale(name): #thread proof function to work with locale
+    with LOCALE_LOCK:
+        saved = locale.setlocale(locale.LC_ALL)
+        try:
+            yield locale.setlocale(locale.LC_ALL, name)
+        finally:
+            locale.setlocale(locale.LC_ALL, saved)
+
+icon_lookup = {
+    'clear-day': "assets/Sun.png",  # clear sky day
+    'wind': "assets/Wind.png",   #wind
+    'cloudy': "assets/Cloud.png",  # cloudy day
+    'partly-cloudy-day': "assets/PartlySunny.png",  # partly cloudy day
+    'rain': "assets/Rain.png",  # rain day
+    'snow': "assets/Snow.png",  # snow day
+    'snow-thin': "assets/Snow.png",  # sleet day
+    'fog': "assets/Haze.png",  # fog day
+    'clear-night': "assets/Moon.png",  # clear sky night
+    'partly-cloudy-night': "assets/PartlyMoon.png",  # scattered clouds night
+    'thunderstorm': "assets/Storm.png",  # thunderstorm
+    'tornado': "assests/Tornado.png",    # tornado
+    'hail': "assests/Hail.png"  # hail
+}
+
+
+class Clock(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, bg='black')
+        # initialize time label
+        self.time1 = ''
+        self.timeLbl = Label(self, font=('Helvetica', large_text_size), fg="white", bg="black")
+        self.timeLbl.pack(side=TOP, anchor=E)
+        # initialize day of week
+        self.day_of_week1 = ''
+        self.dayOWLbl = Label(self, text=self.day_of_week1, font=('Helvetica', small_text_size), fg="white", bg="black")
+        self.dayOWLbl.pack(side=TOP, anchor=E)
+        # initialize date label
+        self.date1 = ''
+        self.dateLbl = Label(self, text=self.date1, font=('Helvetica', small_text_size), fg="white", bg="black")
+        self.dateLbl.pack(side=TOP, anchor=E)
+        self.tick()
+
+    def tick(self):
+        with setlocale(ui_locale):
+            if time_format == 12:
+                time2 = time.strftime('%I:%M %p') #hour in 12h format
+            else:
+                time2 = time.strftime('%H:%M') #hour in 24h format
+
+            day_of_week2 = time.strftime('%A')
+            date2 = time.strftime(date_format)
+            # if time string has changed, update it
+            if time2 != self.time1:
+                self.time1 = time2
+                self.timeLbl.config(text=time2)
+            if day_of_week2 != self.day_of_week1:
+                self.day_of_week1 = day_of_week2
+                self.dayOWLbl.config(text=day_of_week2)
+            if date2 != self.date1:
+                self.date1 = date2
+                self.dateLbl.config(text=date2)
+            # calls itself every 200 milliseconds
+            # to update the time display as needed
+            # could use >200 ms, but display gets jerky
+            self.timeLbl.after(200, self.tick)
+
 
 #create the window
-root = Tk()
+class FullWindow:
+    def __init__(self):
+        self.tk = Tk() #create window
+        self.tk.title("UMBC IEEE")
+        self.tk.geometry("528x686")
+        self.tk.configure(background='black')
+        self.topFrame = Frame(self.tk, background = 'black')
+        self.bottomFrame = Frame(self.tk, background = 'black')
+        self.topFrame.pack(side = TOP, fill=BOTH, expand = YES)
+        self.bottomFrame.pack(side = BOTTOM, fill=BOTH, expand = YES)
+        #self.state = False
+        #self.tk.bind("<Return>", self.toggle_fullscreen)
+        #self.tk.bind("<Escape>", self.end_fullscreen)
+        # clock
+        self.clock = Clock(self.topFrame)
+        self.clock.pack(side=RIGHT, anchor=N, padx=100, pady=60)
 
-#modify root window
-root.title("Smart Mirror")
-root.geometry("528x686")
+
+
 
 #kick off the event loop
-root.mainloop()
+if __name__ == '__main__':
+    w = FullWindow()
+    w.tk.mainloop()
