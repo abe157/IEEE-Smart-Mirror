27db57029c35c730c05c4687eed6a6d4c1eeee3e
abe157 <abenezertgs96@gmail.com>
2017-11-24 11:21:49 +0000

    Made test2

---
+++ test2.py
@@ -0,0 +1,13 @@
+#simple GUI
+
+from Tkinter import *
+
+#create the window
+root = Tk()
+
+#modify root window
+root.title("Smart Mirror")
+root.geometry("528x686")
+
+#kick off the event loop
+root.mainloop()
